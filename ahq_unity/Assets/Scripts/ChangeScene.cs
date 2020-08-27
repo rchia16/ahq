8a9ef16e92623d3b4ddedf79620a24d2ab0ae419
13035516 <13035516@student.uts.edu.au>
2020-08-27 21:48:22 +1000

    Made a script that changes scene by public string variable

---
+++ ahq_unity/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string SceneName;
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+        Debug.Log("Hello I am changing scene");
+    }
+}
