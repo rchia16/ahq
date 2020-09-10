880d33cb7bf3638ddb8b9501464b0cd678625b81
edwina360ks <45251112+edwina360ks@users.noreply.github.com>
2020-09-10 22:02:23 +1000

    Added a text field
    added a text field

---
+++ ahq_unity/Assets/Scripts/TextField.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextField : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public string stringToEdit = "Tell me a story";
+
+    void OnGUI()
+    {
+        // Make a text field that modifies stringToEdit.
+        stringToEdit = GUI.TextField(new Rect(10, 10, 200, 20), stringToEdit, 25);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
